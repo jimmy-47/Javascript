1d803ecea879153e51495d25a819e207f08e1704
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-04 06:28:26 +0000

    Conversion

---
+++ 01_Basics/03_conversionOperators.js
@@ -0,0 +1,14 @@
+let number=2342
+let convertToString=String(number)
+//console.log(typeof convertToString);
+let string="abcd"
+let convertToNumber=Number(string)
+//console.log(typeof convertToNumber);  // convert to number nut if we print we got output NAN
+
+let isLoggedIn=1
+let convertToBoolean=Boolean(isLoggedIn)
+//console.log(typeof convertToBoolean);   // convert to boolean and ouput is true or false
+
+
+
+
