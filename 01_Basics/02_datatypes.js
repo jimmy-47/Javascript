b557fb03628f9372bd1de4fb5d8c03290cb7ec69
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-04 06:19:41 +0000

    Data Types

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"Use Strict"   // to use newer version of javascript
+
+// Data Types//
+
+/* 
+Number   => range(2 ki power 55)
+Bigint
+Boolean => True /false OR 0/1
+Null=> Standalone value   // Type Object
+Undefined=> Type undefined 
+
+
+*/
