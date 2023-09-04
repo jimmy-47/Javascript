bdf924cce2a9cbca79b3e6eeb0eea1362584a2d4
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-04 10:03:36 +0000

    Initilization and Concatenation of stings

---
+++ 01_Basics/06_Strings.js
@@ -0,0 +1,29 @@
+// ************************* Strings **************************
+
+// ***************** Initilization *******************
+
+//1--- 
+
+const name=" Jimmy "
+
+//    OR
+
+// 2----
+
+const newName=new String(" Jameel ")
+
+// Check time 
+// console.log(name);
+// console.log(newName);
+
+
+// ******************* Concatenation **********************
+
+//1---
+
+console.log("Hello"+name+newName);  // Bad practice // Old practice 
+
+
+//2---
+
+console.log(`Hello My Name is ${name } and my new is ${newName}`);   // Best Practice ocf Concatenation
