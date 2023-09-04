3270de4e63d75a1a3e20b34ea4c09bf41d1f82e6
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-04 05:54:53 +0000

    01_Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=1234567;
+let accountName="Jimmy"
+var accountEamil="jimmy@google.com"
+accountCity="Egypt"
+
+
+console.table([accountId, accountName,accountEamil,accountCity]);
+
+
+// let try to change
+
+//accountId=23456   : This could not change because contant cannt be change once they assigned 
+
+accountName="Jameel"
+accountEamil="jameel@google.com"
+accountCity="Canada"
+
+
+console.table([accountId, accountName,accountEamil,accountCity]);
