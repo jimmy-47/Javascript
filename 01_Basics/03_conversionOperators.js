492f7ee1687abbf6a04c200590205aa743f38851
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-04 06:40:29 +0000

    Opeartors

--- 01_Basics/03_conversionOperators.js
+++ 01_Basics/03_conversionOperators.js
@@ -9,6 +9,30 @@ let isLoggedIn=1
 let convertToBoolean=Boolean(isLoggedIn)
 //console.log(typeof convertToBoolean);   // convert to boolean and ouput is true or false
 
+/******************************  > Operators<*******************************/
 
+/* 
+2+2  => Addition
+2-2  => Subtraction
+2*2  => Multiplication
+2**2 => Power
+2/2  => Division
+2%2  => Modolu // for reminder
+
+// Don't use code like   because it effects code readablity
+
+console.log(+true)
+
+OR
+
+let num1,num2,num3;
+
+num1=num2=num3=2+2
+
+
+
+
+
+*/
 
 
