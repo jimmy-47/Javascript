243745f416e017f2111f0aed769b1d8bb7e88394
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-04 17:01:43 +0000

    Math and numbers

---
+++ 01_Basics/07_numbersAndMath.js
@@ -0,0 +1,33 @@
+// ****************************  Numbers ****************************
+
+// **************************  Initilization ************************
+
+let num=1234
+
+// OR 
+
+let num2=new Number(2342)
+
+//console.log(num2);
+
+// ****************** Methods ************************
+
+//console.log(num2.toFixed(2))  // fixed the value of decimal value  usually used for the ecommerece website
+
+/// ++++++++++++++++++++++++++++++ Math for JavaScript ++++++++++++++++++++++++++++
+
+//console.log(Math.random())  // usually its value lies between 0 and 1 it could be any decimal value 
+
+//console.log((Math.random() * 10)+1)  // to avoid the repetation of zero  it provide any value greater than zero
+
+let min=1;
+let max=6;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))  // craeting a random generator with min and max function 
+
+// Math.floor (round of to minimum)  e.g 4.9==4
+
+console.log(Math.ceil(Math.random()))   // ciel round the number to maximun  e.g 4.1 ==5
+
+
+
