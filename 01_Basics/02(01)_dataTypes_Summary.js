b01edd225cf8b655c96082fe3836a4ea55a5a7a8
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-04 08:01:25 +0000

    DataTypes Summary With Decleartions

---
+++ 01_Basics/02(01)_dataTypes_Summary.js
@@ -0,0 +1,37 @@
+//  **********************  Data Types  ****************************
+
+/* 
+  1- Primitive data Types 
+
+  String , Number , Boolean , Null , undefined , Symbol , BigInt
+
+  2- Non Primitive (Reference )
+
+  Array Objects Function 
+*/
+
+
+// ******************* declearation Of Primitive *********************
+
+let String="abcd"   // Strings
+let number=124   // Numbers
+let userLoggedIn=true   // Boolean
+let outSideTemp=null  // Null
+let id=Symbol('234')  // Symbol
+let bigNumber=23233233232323n   // BigInt (last pa n lagane sa bigInt mein convert ho jata hai)
+let undefinedDeclartion;   // undeined 
+
+
+
+//***************** Non Primitive **********************/
+// Array
+const fruits=["Apple", "Banana" ," Adam"]
+//Objects
+let objDecleartion={
+    name: "Jimmy",
+    age: 24,
+}
+//Functions
+let myFunction=function(){
+    console.log("Helloo MyFunction");
+}
