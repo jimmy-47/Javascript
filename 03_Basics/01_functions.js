9e937105b16f86b1ea0909e04c89831bb98b7b8d
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-05 14:10:00 +0000

    Rest Operator objects and array in function

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -25,4 +25,42 @@ const userLoggedIn=function(username){  // if the user passes the name
 const userLoggedInn=function(username ="Customer"){  // if user did not passes any thing it automatically get this name 
     return`${username} is logged in`
 }
-console.log(userLoggedInn());
+// console.log(userLoggedInn());
+
+
+// ******************************** Rest Operator ****************************
+   // rest operator and spread operators are same 
+
+function valueOfCart(...noOfItems){    // this is spread operator and return the value in the array
+    return noOfItems;
+}
+// console.log(valueOfCart(1000,23,"hell",54));
+
+
+
+
+
+// *****************************  Passing an object  value in function *********************
+
+const userProfile={
+    name:"Jimmy",
+    age:23,
+    email:"jimmy@yahoo.com"
+}
+
+function objValues(userValues){    // passing the object
+    return `Hello ${userValues.name} your age is${userValues.age} and your email is ${userValues.email}`  // return the statement
+
+}
+
+// console.log(objValues(userProfile));
+
+//***************************** Same could be done with array**************************** */
+
+const fruits=["Apple", "Jerry" ,"Anar"]
+
+function returnSecondPosition(fruits){
+    return fruits[1];
+}
+console.log(returnSecondPosition(fruits));
+
