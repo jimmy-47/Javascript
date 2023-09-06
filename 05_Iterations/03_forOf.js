31c8a82fd91c678992e418456fda5cdeb40074c1
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-06 17:47:47 +0000

    for of

---
+++ 05_Iterations/03_forOf.js
@@ -0,0 +1,13 @@
+//***********************  For Of ***************** */
+
+// Syntax
+
+// const element=[2,3,4,5,6];    // for in synatx
+
+// for (const i of element) {
+//     console.log(i)
+// }
+
+
+// ****************** for in *********************
+
