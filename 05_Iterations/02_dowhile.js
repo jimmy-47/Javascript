d8ed5fc267cc884a5005237ad24ccb16734c4427
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-06 09:42:59 +0000

    Do While and while loop

---
+++ 05_Iterations/02_dowhile.js
@@ -0,0 +1,23 @@
+//********************* DO While Loops************* */
+
+
+// Basic syntax 
+
+// do {
+    
+// } while (condition);
+
+let score=10;    // if condition does not match also do while run atleast once 
+// do {
+//     console.log(score);
+//     score++;
+// } while (score<20);
+
+
+
+//**********************************  While Loop ******************** */
+
+while (score <20) {   // it check condition and print
+    console.log(score);
+    score++;
+}
