f54081076a1dd03109108feeed3df114e48d5ddb
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-06 23:42:45 +0530

    Update 03_forOf.js

--- 05_Iterations/03_forOf.js
+++ 05_Iterations/03_forOf.js
@@ -9,5 +9,29 @@
 // }
 
 
-// ****************** for in *********************
+// ****************** for in   for objects*********************
+const languages={
+      js:"javascript",
+      py:"python",
+      ry:"ruby"
+}
+for (const name in languages) {
+//   console.log(`${name} for the ${languages[name]}`)
+}
 
+// *****************  for in for arrays **************
+
+const lang=["python","ruby","c++","Javascript"]
+for (const index in lang) {
+//    console.log(lang[index])
+}
+
+//**********  Map In javascript********** */
+
+const map=new Map()
+map.set('IN','India'),
+map.set('FR','France')
+
+for (const [key,value] of map) {
+   console.log(key,':-',value)
+}
