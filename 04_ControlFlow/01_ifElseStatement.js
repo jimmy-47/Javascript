6092f01ec290f401096cd1faa661567ba247b0d3
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-06 07:18:14 +0000

    if else

---
+++ 04_ControlFlow/01_ifElseStatement.js
@@ -0,0 +1,47 @@
+// ********************** Declaration *****************
+
+// if (condition) {   // simple if 
+    
+// }
+
+// if (condition) {    // if else 
+    
+// } else {
+    
+// }
+
+
+//*************  Nested If else ********** */
+
+// if (condition) {
+    
+// } else if(){
+    
+// }
+// else{
+
+// }
+
+
+// ********************** Logical Operator *********************
+
+// &&  // check all condition should be true then work
+
+// ||  // anyone condition should be true
+
+// ? : // Ternary operator // if(true) ?console.log(true):console.log(false)
+
+// ******************** Opeartors ******************
+
+/* 
+>   greater than
+<   less than
+<=   less than equal to
+>=   greater than equal to
+==   comparasion
+===  strict comparsion
+!=  not equal
+!==  strict not equal
+
+
+*/
