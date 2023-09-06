35e1e0f0b0892d4f78ca114798081ad2b0bddbc6
Jameel Chowdhary <114846589+jimmy-47@users.noreply.github.com>
2023-09-06 07:20:31 +0000

    switch

---
+++ 04_ControlFlow/02_switch.js
@@ -0,0 +1,11 @@
+const month="monday"
+
+switch (month) {       // key is name & value is value that is assigned to variable 
+    case "monday":
+        console.log("Monday");
+        break;
+
+    default:
+        console.log("nothing");
+        break;
+}
