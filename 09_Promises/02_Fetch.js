2e46a23a2cafdd44ee3356c62db6fdfbb2025e6d
Jameel Chowdhary <jc.clickbit@gmail.com>
2023-09-09 17:56:48 +0530

    Fetch

---
+++ 09_Promises/02_Fetch.js
@@ -0,0 +1,9 @@
+fetch('https://api.github.com/users/jimmy-47')
+.then((response)=>{
+    return response.json()
+}).then((data)=>{
+    console.log(data);
+    console.log(data.followers);
+}).catch(()=>{
+    console.log('oops got error');
+})
